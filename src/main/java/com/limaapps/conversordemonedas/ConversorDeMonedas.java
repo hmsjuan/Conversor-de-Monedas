443f395ba7ff3bf228df3dde428483bc72983ab6
Humberto Suarez <segundafuente@outlook.com>
2024-04-19 22:16:04 -0400

    Creando la funcion para obtener la tasa de Cambio, en este caso nomando como base la moneda DOP-Peso Dominicano, usando la API de ExchangeRate. Se importo > com.google.gson.JsonParser;

--- src/main/java/com/limaapps/conversordemonedas/ConversorDeMonedas.java
+++ src/main/java/com/limaapps/conversordemonedas/ConversorDeMonedas.java
@@ -1,4 +1,34 @@
 package com.limaapps.conversordemonedas;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ConversorDeMonedas {
+    public String reqResultado="";
+    Map<String, Double> tasaDeCambio = new HashMap<>();
+
+    public void obtenerTasasDeCambio(String monedaLocal) throws IOException {
+        // Setting URL
+        String url_str = "https://v6.exchangerate-api.com/v6/579d93f1562091d14c9eef2f/latest/" + monedaLocal;
+
+        // Making Request
+        URL url = new URL(url_str);
+        HttpURLConnection request = (HttpURLConnection) url.openConnection();
+        request.connect();
+        // Convert to JSON
+        JsonParser jp = new JsonParser();
+        JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+        JsonObject jsonobj = root.getAsJsonObject();
+
+        reqResultado = jsonobj.get("result").getAsString();
+    }
 }
