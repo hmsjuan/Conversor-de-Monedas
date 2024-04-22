3ed68d145c1cb329c551a0cb808f25e9c7a1ce5a
Humberto Suarez <segundafuente@outlook.com>
2024-04-22 17:26:30 -0400

    Simplificación del código eliminando duplicaciones.  Uso de un bucle do-while para evitar la repetición de código.  Definición de una función para obtener la moneda de destino basada en la opción seleccionada.

--- src/main/java/com/limaapps/conversordemonedas/ConversorDeMonedas.java
+++ src/main/java/com/limaapps/conversordemonedas/ConversorDeMonedas.java
@@ -1,6 +1,5 @@
 package com.limaapps.conversordemonedas;
 
-
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -15,52 +14,44 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ConversorDeMonedas {
-    public String reqResultado="";
+    public String reqResultado = "";
     Map<String, Double> tasaDeCambio = new HashMap<>();
 
     public void obtenerTasasDeCambio(String monedaLocal) throws IOException {
-        // Setting URL
-        String url_str = "https://v6.exchangerate-api.com/v6/579d93f1562091d14c9eef2f/latest/" + monedaLocal;
+        String url_str = "https://v6.exchangerate-api.com/v6/667dbfc4f3b7ca3754b9e386/latest/" + monedaLocal;
 
-        // Making Request
         URL url = new URL(url_str);
         HttpURLConnection request = (HttpURLConnection) url.openConnection();
         request.connect();
-        // Convert to JSON
+
+        // Convertir a JSON
         JsonParser jp = new JsonParser();
         JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
         JsonObject jsonobj = root.getAsJsonObject();
 
         reqResultado = jsonobj.get("result").getAsString();
 
-        if (!reqResultado.equals("success")){
-            //errores
-        }else{
-            String valoresModenas= String.valueOf(jsonobj.get("conversion_rates"));
-            // Elimina los caracteres de llaves y comas
-            valoresModenas = valoresModenas.replace("{", "").replace("}", "");
-
-            String[] keyValuePairs = valoresModenas.split(",");
-
-            for (String pair : keyValuePairs) {
-                String[] keyValue = pair.split(":");
-                String key = keyValue[0].replace("\"", ""); // Elimina las comillas
-                double value = Double.parseDouble(keyValue[1]);
+        if (!reqResultado.equals("success")) {
+            // manejar errores
+        } else {
+            JsonObject conversionRates = jsonobj.getAsJsonObject("conversion_rates");
+            for (Map.Entry<String, JsonElement> entry : conversionRates.entrySet()) {
+                String key = entry.getKey();
+                double value = entry.getValue().getAsDouble();
                 tasaDeCambio.put(key, value);
             }
-
         }
     }
 
     public String cambiarMoneda(String monedaOrigen, String monedaDestino, double cantidad) {
         DecimalFormat df = new DecimalFormat("0.000");
-        double resutado = 0;
+        double resultado = 0;
         double valorOrigen = tasaDeCambio.get(monedaOrigen);
         double valorDestino = tasaDeCambio.get(monedaDestino);
 
-        resutado= (valorDestino/valorOrigen) * (cantidad);
+        resultado = (valorDestino / valorOrigen) * cantidad;
 
         return String.format("El valor %s [%s] corresponde al valor final de =>>> $%s [%s]\n",
-                df.format(cantidad),monedaOrigen,df.format(resutado),monedaDestino);
+                df.format(cantidad), monedaOrigen, df.format(resultado), monedaDestino);
     }
 }
--- src/main/java/com/limaapps/conversordemonedas/Main.java
+++ src/main/java/com/limaapps/conversordemonedas/Main.java
@@ -3,22 +3,16 @@ package com.limaapps.conversordemonedas;
 import java.io.IOException;
 import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
-public class Main
-{
+
+public class Main {
     public static void main(String[] args) throws IOException {
-        //Se puede indicar la moneda local para que la herramienta funciones en cualquira que indiques-
-        String monedaLocal="DOP";
-        String montoConvertido = "**";
+        String monedaLocal = "DOP";
         Scanner teclado = new Scanner(System.in);
-        int opcion = 500;
-        double montoAConvertir=0;
+        String monedaDestino;
+        String monedaOrigen = "";
 
         ConversorDeMonedas conversorDeMonedas = new ConversorDeMonedas();
-        if (!conversorDeMonedas.reqResultado.equals("success")){
+        if (!conversorDeMonedas.reqResultado.equals("success")) {
             conversorDeMonedas.obtenerTasasDeCambio(monedaLocal);
         }
 
@@ -42,72 +36,49 @@ public class Main
                Escriba una opción válida:
                 """.formatted(monedaLocal);
 
-        String cadenaParaInput = "Cuàl es el valor en ["+monedaLocal+"] que desea convertir a: ";
-        while (opcion!=0) {
+
+        int opcion;
+        do {
             System.out.print(opciones);
             opcion = teclado.nextInt();
 
-            if (opcion>=1 && opcion<=10){
-                switch (opcion){
-                    case 1:
-                        System.out.print(cadenaParaInput+"[USD]");
-                        montoAConvertir = teclado.nextDouble();
-                        montoConvertido = conversorDeMonedas.cambiarMoneda(monedaLocal,"USD", montoAConvertir);
-                        break;
-                    case 2:
-                        System.out.print(cadenaParaInput+"[EUR]");
-                        montoAConvertir = teclado.nextDouble();
-                        montoConvertido = conversorDeMonedas.cambiarMoneda(monedaLocal,"EUR", montoAConvertir);
-                        break;
-                    case 3:
-                        System.out.print(cadenaParaInput+"[GBP]");
-                        montoAConvertir = teclado.nextDouble();
-                        montoConvertido = conversorDeMonedas.cambiarMoneda(monedaLocal,"GBP", montoAConvertir);
-                        break;
-                    case 4:
-                        System.out.print(cadenaParaInput+"[JPY]");
-                        montoAConvertir = teclado.nextDouble();
-                        montoConvertido = conversorDeMonedas.cambiarMoneda(monedaLocal,"JPY", montoAConvertir);
-                        break;
-                    case 5:
-                        System.out.print(cadenaParaInput+"[KRW]");
-                        montoAConvertir = teclado.nextDouble();
-                        montoConvertido = conversorDeMonedas.cambiarMoneda(monedaLocal,"KRW", montoAConvertir);
-                        break;
-                    case 6:
-                        System.out.print("Cuál es el valor en [USD] que desea convertir a["+monedaLocal+"]: ");
-                        montoAConvertir = teclado.nextDouble();
-                        montoConvertido = conversorDeMonedas.cambiarMoneda("USD",monedaLocal, montoAConvertir);
-                        break;
-                    case 7:
-                        System.out.print("Cuál es el valor en [EUR] que desea convertir a["+monedaLocal+"]: ");
-                        montoAConvertir = teclado.nextDouble();
-                        montoConvertido = conversorDeMonedas.cambiarMoneda("EUR",monedaLocal, montoAConvertir);
-                        break;
-                    case 8:
-                        System.out.print("Cuál es el valor en [GBP] que desea convertir a["+monedaLocal+"]: ");
-                        montoAConvertir = teclado.nextDouble();
-                        montoConvertido = conversorDeMonedas.cambiarMoneda("GBP",monedaLocal, montoAConvertir);
-                        break;
-                    case 9:
-                        System.out.print("Cuál es el valor en [JPY] que desea convertir a["+monedaLocal+"]: ");
-                        montoAConvertir = teclado.nextDouble();
-                        montoConvertido = conversorDeMonedas.cambiarMoneda("JPY",monedaLocal, montoAConvertir);
-                        break;
-                    case 10:
-                        System.out.print("Cuál es el valor en [KRW] que desea convertir a["+monedaLocal+"]: ");
-                        montoAConvertir = teclado.nextDouble();
-                        montoConvertido = conversorDeMonedas.cambiarMoneda("KRW",monedaLocal, montoAConvertir);
-                        break;
+            if (opcion >= 1 && opcion <= 10) {
+                if (opcion >=1 && opcion<=5){
+                    monedaDestino = obtenerMonedaDestino(opcion);
+                    monedaOrigen=monedaLocal;
+                }else{
+                    monedaOrigen =obtenerMonedaDestino(opcion-5);
+                    monedaDestino = monedaLocal;
                 }
+                String cadenaParaInput = "Cuál es el valor en [" + monedaOrigen + "] que desea convertir a["+monedaDestino+"]  : ";
+                System.out.print(cadenaParaInput);
+
+                double montoAConvertir = teclado.nextDouble();
+                String montoConvertido = conversorDeMonedas.cambiarMoneda(monedaOrigen, monedaDestino, montoAConvertir);
                 System.out.println(montoConvertido);
-            }else if (opcion==0){
+            } else if (opcion == 0) {
                 System.out.println("\n*> Ha optado por salir del sistema. * ");
-            }else{
+            } else {
                 System.out.println("OPCIÓN NO VÁLIDA");
             }
+        } while (opcion != 0);
+    }
 
+    private static String obtenerMonedaDestino(int opcion) {
+        switch (opcion) {
+            case 1:
+                return "USD";
+            case 2:
+                return "EUR";
+            case 3:
+                return "GBP";
+            case 4:
+                return "JPY";
+            case 5:
+                return "KRW";
+
+            default:
+                return "";
         }
-
     }
 }
